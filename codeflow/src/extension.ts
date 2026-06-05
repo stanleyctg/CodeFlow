85180fc3cdb46de9d3d917ae265b4a4cfa8b815a
stanleyctg <stanleyctg@yahoo.com>
2026-06-05 13:22:42 +0100

    Initialise project

---
+++ codeflow/src/extension.ts
@@ -0,0 +1,33 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "codeflow" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('codeflow.displayWelcomeMessage', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Welcome to CodeFlow!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	const timeDisposable = vscode.commands.registerCommand('codeflow.displayCurrentTime', () => {
+		const currentTime = new Date().toLocaleTimeString();
+		vscode.window.showErrorMessage(`Current Time: ${currentTime}`);
+	});
+	
+	context.subscriptions.push(timeDisposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
