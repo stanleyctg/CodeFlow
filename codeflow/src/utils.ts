1b8728f9cde8e4f0f039a61786cbd74e4c1655d8
stanleyctg <stanleyctg@yahoo.com>
2026-06-14 16:25:06 +0100

    Use ternary operator instead of if/else

--- codeflow/src/utils.ts
+++ codeflow/src/utils.ts
@@ -2,9 +2,5 @@ import * as vscode from 'vscode';
 
 export function getSelectedText(editor: vscode.TextEditor): string {
     const selection = editor.document.getText(editor.selection);
-    if (selection) {
-        return selection;
-    } else {
-        return '';
-    }
+    return selection ? selection : '';
 }
