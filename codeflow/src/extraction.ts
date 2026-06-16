518a5eb85c8e6250133dd91ebfd6b91eaae22ba9
stanleyctg <stanleyctg@yahoo.com>
2026-06-16 13:43:23 +0100

    Implement extract files from workspace

--- codeflow/src/extraction.ts
+++ codeflow/src/extraction.ts
@@ -1,3 +1,4 @@
+import * as vscode from 'vscode';
 import { FunctionInfo } from './types';
 
 export function extractFunctionsFromFiles(files: string[]): FunctionInfo[] {
@@ -6,4 +7,10 @@ export function extractFunctionsFromFiles(files: string[]): FunctionInfo[] {
         file: 'ignore.py',
         class: 'ignore'
     }];
+}
+
+// Dep inject with vscode.workspace.findFiles to make it easier to test
+export async function extractFilesFromWorkspace(findFiles: (pattern: string) => Thenable<vscode.Uri[]>): Promise<string[]> {
+    const uris = await findFiles('**/*.py');
+    return uris.map(uri => uri.fsPath);
 }
--- codeflow/src/test/extraction.test.ts
+++ codeflow/src/test/extraction.test.ts
@@ -1,14 +1,74 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { extractFunctionsFromFiles } from '../extraction';
+import { extractFunctionsFromFiles, extractFilesFromWorkspace } from '../extraction';
 
 function fixturesPath(filename: string): string {
     return path.join(__dirname, 'fixtures', filename);
 }
 
 
-suite('Extraction Test Suite', () => {
+suite('File Extraction Test Suite', () => {
+    vscode.window.showInformationMessage('Start Extraction tests.');
+
+    // test 1 extract all files from workspace
+    test('extract all files from workspace folder', async() => {
+        const findFilesMock = async (pattern: string): Promise<vscode.Uri[]> => {
+            return [
+                vscode.Uri.file(fixturesPath('test_workspace/1-.py')),
+                vscode.Uri.file(fixturesPath('test_workspace/2-.py')),
+                vscode.Uri.file(fixturesPath('test_workspace/3-.py'))
+            ];
+        };
+        const result = await extractFilesFromWorkspace(findFilesMock);
+        assert.deepStrictEqual(result, [
+            fixturesPath('test_workspace/1-.py'),
+            fixturesPath('test_workspace/2-.py'),
+            fixturesPath('test_workspace/3-.py')
+        ]);
+    });
+
+
+    // test 2 pop error message if workspace doesnt exist
+    test('pop error message if workspace doesnt exist', async() => {
+        const findFilesMock = async (pattern: string): Promise<vscode.Uri[]> => {
+            throw new Error('Workspace not found');
+        };
+        await assert.rejects(async () => {
+            await extractFilesFromWorkspace(findFilesMock);
+        }, {
+            message: 'Workspace not found'
+        });
+    });
+
+    // test 3 return empty array if no files found in the workspace
+    test('return empty array if no files found in the workspace', async() => {
+        const findFilesMock = async (pattern: string): Promise<vscode.Uri[]> => {
+            return [];
+        };
+        const result = await extractFilesFromWorkspace(findFilesMock);
+        assert.deepStrictEqual(result, []);
+    });
+
+    // test 4 return all files in workspace, including files in subfolders
+    test('return all files in workspace, including files in subfolders', async() => {
+        const findFilesMock = async (pattern: string): Promise<vscode.Uri[]> => {
+            return [
+                vscode.Uri.file(fixturesPath('test_workspace_subfolder/1-.py')),
+                vscode.Uri.file(fixturesPath('test_workspace_subfolder/2-.py')),
+                vscode.Uri.file(fixturesPath('test_workspace_subfolder/test-subfolder/1-.py'))
+            ];
+        };
+        const result = await extractFilesFromWorkspace(findFilesMock);
+        assert.deepStrictEqual(result, [
+            fixturesPath('test_workspace_subfolder/1-.py'),
+            fixturesPath('test_workspace_subfolder/2-.py'),
+            fixturesPath('test_workspace_subfolder/test-subfolder/1-.py')
+        ]);
+    });
+});
+
+suite('Function Extraction Test Suite', () => {
     vscode.window.showInformationMessage('Start Extraction tests.');
 
     test.skip('extracts all functions from a file', async() => {
